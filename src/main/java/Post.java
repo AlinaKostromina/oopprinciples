1639d68b313850d3e16462f2071f6ad3d9e1d6b2
Kostromina Alina <a.kstrmna95@gmail.com>
2023-10-03 20:44:04 +0700

    Task resolved

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для номера паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public Boolean subscription; // поле для определения подписки (да/нет)
+    public FormDate birthday; // поле для даты ДР (из класса FormDate)
+}
